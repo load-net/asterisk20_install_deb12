9fd3609776744dce3f43419a2d157e9b7cc2160a
load-net <53649615+load-net@users.noreply.github.com>
2024-02-05 20:38:56 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,25 +5,12 @@ cd /usr/src/asterisk-20.6.0/
 contrib/scripts/install_prereq install
 echo ################ ./configure --with-pjproject-bundled ###################################
 cd /usr/src/asterisk-20.6.0/
-./configure --with-pjproject-bundled
-echo ####################### menuselect ###################################
-for ((i=0; i<5; i++))
-do
-    echo -n "#"
-    sleep 2
-done
-cd /usr/src/asterisk-20.6.0/
-for ((i=0; i<5; i++))
-do
-    echo -n "."
-    sleep 2
-done
-echo #######
-echo #######
-echo #######
-sleep 2
-cd /usr/src/asterisk-20.6.0/
-menuselect/menuselect --disable BUILD_NATIVE --disable-all \
+	&& yes | contrib/scripts/install_prereq install \
+	&& contrib/scripts/install_prereq test \
+	&& ./bootstrap.sh && ./configure --with-pjproject-bundled \
+	&& make menuselect.makeopts \
+	&& menuselect/menuselect
+		--disable BUILD_NATIVE --disable-all \
 		--enable chan_bridge_media \
 		--enable chan_rtp \
 		--enable chan_alsa \
